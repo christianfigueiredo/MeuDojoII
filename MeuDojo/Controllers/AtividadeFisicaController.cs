b1bb034989140177b5790e6dd6d793d845accd04
christian_emanick <emanick3@gmail.com>
2021-02-16 21:50:27 +0000

    criação dos menus e submenus

---
+++ MeuDojo/Controllers/AtividadeFisicaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeuDojo.Controllers
+{
+    public class AtividadeFisicaController : Controller
+    {
+        // GET: AtividadeFisica
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MeuDojo/Controllers/RecepcaoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeuDojo.Controllers
+{
+    public class RecepcaoController : Controller
+    {
+        // GET: Recepcao
+        public ActionResult PreCadastro()
+        {
+            return View();
+        }
+        public ActionResult Cliente()
+        {
+            return View();
+        }
+        public ActionResult MapaDeCliente()
+        {
+            return View();
+        }
+        public ActionResult AgendaDeTurma()
+        {
+            return View();
+        }
+        public ActionResult AgendaDeServico()
+        {
+            return View();
+        }
+        public ActionResult AulaExperimental()
+        {
+            return View();
+        }
+    }
+}
